73103896a90b906bb299b76d7cdb829f0cdd0522
Rebecca Nesson <rebeccanesson@gmail.com>
2015-01-02 16:17:33 -0500

    changing things to work with vimeo

--- app/controllers/vimeo_embeds_controller.rb
+++ app/controllers/vimeo_embeds_controller.rb
@@ -2,9 +2,9 @@ class VimeoEmbedsController < ApplicationController
 
   def create
     video_id = params[:url]
-    iframe_url = Rack::Utils.escape("https://www.youtube.com/embed/#{video_id}")
+    iframe_url = Rack::Utils.escape("https://player.vimeo.com/video/#{video_id}?portrait=0&amp;badge=0")
     logger.info 'IFRAME URL : ' + iframe_url
-    redirect_url = session[:launch_presentation_return_url] + '?return_type=iframe&url=' + iframe_url + '&width=560&height=315'
+    redirect_url = session[:launch_presentation_return_url] + '?return_type=iframe&url=' + iframe_url + '&width=500&height=281'
     redirect_to redirect_url
   end
 
